377fb063d682000bbed706d2d41c3eb330faf557
Chaqys <maxkurylevv@gmail.com>
2024-11-13 17:27:32 +0700

    Add files via upload

--- Module_12_2/module_12_2.py
+++ Module_12_2/module_12_2.py
@@ -40,41 +40,47 @@ class Tournament:
         return finishers
 
 
-class TestTournament(unittest.TestCase):
+import runner_and_tournament as run_turn
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
 
     def setUp(self):
-        self.runner1 = Runner("Усэйн", speed=10)
-        self.runner2 = Runner("Андрей", speed=9)
-        self.runner3 = Runner("Ник", speed=3)
+        self.runners = [
+            run_turn.Runner('Усэйн', 10),
+            run_turn.Runner('Андрей', 9),
+            run_turn.Runner('Ник', 3),
+        ]
 
-    def test_race_usain_and_nik(self):
-        tournament = Tournament(90, self.runner1, self.runner3)
-        results = tournament.start()
-        formatted_result = {place: str(runner) for place, runner in results.items()}
-        print(formatted_result)  # Выводим результат
-        self.assertEqual(len(results), 2)
-        self.assertEqual(results[1], self.runner1)
-        self.assertEqual(results[2], self.runner3)
+    @classmethod
+    def tearDownClass(cls):
+        for i, f in cls.all_results.items():
+            print("{}:".format(i))
+            for e, d in f.items():
+                print("{}: {}".format(e, d))
 
-    def test_race_andrey_and_nik(self):
-        tournament = Tournament(90, self.runner2, self.runner3)
-        results = tournament.start()
-        formatted_result = {place: str(runner) for place, runner in results.items()}
-        print(formatted_result)  # Выводим результат
-        self.assertEqual(len(results), 2)
-        self.assertEqual(results[1], self.runner2)
-        self.assertEqual(results[2], self.runner3)
+    def test_1(self):
+        turn_1 = run_turn.Tournament(90, self.runners[0], self.runners[2])
+        result = turn_1.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Усейн и Ник"] = result
 
-    def test_race_usain_andrey_and_nik(self):
-        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
-        results = tournament.start()
-        formatted_result = {place: str(runner) for place, runner in results.items()}
-        print(formatted_result)  # Выводим результат
-        self.assertEqual(len(results), 3)
-        self.assertEqual(results[1], self.runner1)
-        self.assertEqual(results[2], self.runner2)
-        self.assertEqual(results[3], self.runner3)
+    def test_2(self):
+        turn_2 = run_turn.Tournament(90, self.runners[1], self.runners[2])
+        result = turn_2.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Андрей и Ник"] = result
 
+    def test_3(self):
+        turn_3 = run_turn.Tournament(90, *self.runners)
+        result = turn_3.start()
+        self.assertTrue(list(result.values())[-1].name == "Ник")
+        self.all_results["Усейн, Андрей и Ник"] = result
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
