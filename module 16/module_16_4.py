699a6f0a4f55667070616378d0a12d4dc1a748a6
Chaqys <maxkurylevv@gmail.com>
2024-12-11 14:44:10 +0700

    Add files via upload

---
+++ module 16/module_16_4.py
@@ -0,0 +1,54 @@
+# Домашнее задание по теме "Модели данных Pydantic".
+
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+app = FastAPI()
+users = []
+
+
+@app.get("/users")
+async def get_users() -> list:
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def post_user(user: User):
+    if not users:
+        new_id = 1
+    else:
+        new_id = users[-1].id + 1
+    users.append(user)
+    return user
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int,
+                      username: str = Path(min_length=5, max_length=20,
+                                           description="Enter username", example='UrbanUser'),
+                      age: int = Path(ge=18, le=120, description="Enter age", example=24)) -> User:
+    try:
+        for user in users:
+            if user.id == user_id:
+                user.username = username
+                user.age = age
+            return user
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int) -> dict[str, str]:
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            users.pop(index)
+            return {'message': 'User deleted'}
+    else:
+        raise HTTPException(status_code=404, detail='User was not found')
